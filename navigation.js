173b78f389d76275c90dc509fc765d2fb0bd2d9e
ClintonYounge <clintonyounge18@gmail.com>
2023-02-24 12:19:50 -0400

    Update html and js
    -Separated the html into 3 sections for the list, add new and contact section.
    
    -add a navbar with nav items to navigate through the 3 sections.

---
+++ navigation.js
@@ -0,0 +1,24 @@
+const listSection = document.querySelector('.list-section');
+const addNewSection = document.querySelector('.add-new-section');
+const contactSection = document.querySelector('.contact-section');
+const listLink = document.querySelector('.link1');
+const addNewLink = document.querySelector('.link2');
+const contactLink = document.querySelector('.link3');
+
+listLink.addEventListener('click', () => {
+    listSection.style.display = 'block';
+    addNewSection.style.display = 'none';
+    contactSection.style.display = 'none';
+});
+
+addNewLink.addEventListener('click', () => {
+    listSection.style.display = 'none';
+    addNewSection.style.display = 'block';
+    contactSection.style.display = 'none';
+});
+
+contactLink.addEventListener('click', () => {
+    listSection.style.display = 'none';
+    addNewSection.style.display = 'none';
+    contactSection.style.display = 'block';
+})
